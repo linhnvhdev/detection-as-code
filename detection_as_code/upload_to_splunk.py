a2d8cf7365d6328fba4f2873a598c13d7da085bf
Le Dung <dung1512002@gmail.com>
2023-09-14 14:26:40 +0700

    fix/env

--- detection_as_code/upload_to_splunk.py
+++ detection_as_code/upload_to_splunk.py
@@ -1,50 +1,81 @@
 import requests
 import urllib3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 urllib3.disable_warnings()
 
-api_url_base="https://localhost:8089"
-headers = {'Content-Type': 'application/json'}
-auth_method="Splunk"
-username="dungbacon"
-password="devforlife011502"
+auth_method = os.getenv('SPLUNK_AUTH_PRE')
+username = os.getenv('SPLUNK_USERNAME')
+password = os.getenv('SPLUNK_PASSWORD')
+base_api_url = os.getenv('SPLUNK_URL_BASE')
+
+real_time_search_schema = {
+    "name": ""
+}
+
+schedule_search_schema = {
+    "name": "",
+    "search": "",
+    "alert_type": "number of events",
+    "alert_comparator": "greater than",
+    "alert_threshold": 0,
+    "is_scheduled": True,
+    "cron_schedule": "*/5 * * * *",
+}
+
 
 def upload(rules):
     print("Upload to Splunk:\n")
-    access_token = auth_method + " " + api_authorize() 
+    print(api_authorize())
+    access_token = auth_method + " " + api_authorize()
     api_upload_rules(rules, access_token)
+
 
 def api_authorize():
     try:
-        authorize_url = api_url_base + "/services/auth/login?output_mode=json"
+        headers = {'Content-Type': 'application/json'}
+        authorize_url = base_api_url + "/services/auth/login?output_mode=json"
+        print(authorize_url)
         data = {'username': username, 'password': password}
-        response = requests.post(authorize_url, headers=headers, data=data, verify=False)
+        response = requests.post(
+            authorize_url, headers=headers, data=data, verify=False)
         return response.json()['sessionKey']
     except:
         print("Error: cannot connect to Splunk")
         return None
 
+
 def api_upload_rules(rules, access_token):
     try:
-        auth_header={'Authorization': access_token}
-        upload_url = api_url_base + "/services/search/jobs"
+        auth_header = {'Authorization': access_token}
+        upload_url = base_api_url + "/services/search/jobs"
 
         for rule in rules:
             print("Search: " + rule.rule[0])
             response = requests.post(
-                upload_url, 
-                headers=auth_header, 
+                upload_url,
+                headers=auth_header,
                 data={
+                    "name": rule.title,
                     "search": "search " + rule.rule[0],
-                }, 
+                },
                 verify=False)
             print(response.text)
             print("\n")
-            if(response.status_code == 201):
-                print("Upload rule ", rule.title, " successfully")
+            if (response.status_code == 201):
+                print("Upload rule", rule.title, "successfully")
             else:
                 print("Error: cannot upload rule ", rule.title)
     except:
         print("Error: cannot connect to Splunk")
-    
-    
+
+
+def api_update_search():
+    pass
+
+
+def api_delete_search():
+    pass
