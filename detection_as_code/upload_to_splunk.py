e469632b956143b945910d62e82da5fbfc8f1d00
Le Dung <dung1512002@gmail.com>
2023-09-15 09:05:57 +0700

    splunk/alert-upload

--- detection_as_code/upload_to_splunk.py
+++ detection_as_code/upload_to_splunk.py
@@ -12,33 +12,19 @@ username = os.getenv('SPLUNK_USERNAME')
 password = os.getenv('SPLUNK_PASSWORD')
 base_api_url = os.getenv('SPLUNK_URL_BASE')
 
-real_time_search_schema = {
-    "name": ""
-}
-
-schedule_search_schema = {
-    "name": "",
-    "search": "",
-    "alert_type": "number of events",
-    "alert_comparator": "greater than",
-    "alert_threshold": 0,
-    "is_scheduled": True,
-    "cron_schedule": "*/5 * * * *",
-}
-
 
 def upload(rules):
     print("Upload to Splunk:\n")
     print(api_authorize())
     access_token = auth_method + " " + api_authorize()
     api_upload_rules(rules, access_token)
+    # api_search_job(rules, access_token)
 
 
 def api_authorize():
     try:
         headers = {'Content-Type': 'application/json'}
         authorize_url = base_api_url + "/services/auth/login?output_mode=json"
-        print(authorize_url)
         data = {'username': username, 'password': password}
         response = requests.post(
             authorize_url, headers=headers, data=data, verify=False)
@@ -51,26 +37,58 @@ def api_authorize():
 def api_upload_rules(rules, access_token):
     try:
         auth_header = {'Authorization': access_token}
-        upload_url = base_api_url + "/services/search/jobs"
-
+        upload_url = base_api_url + "/services/saved/searches?output_mode=json"
         for rule in rules:
-            print("Search: " + rule.rule[0])
+            schema = {
+                "name": rule.title,
+                "search": "index=main eventtype=ms-sysmon-process",
+                "is_scheduled": True,
+                "cron_schedule": "*/5 * * * *",
+                "alert_type": "number of events",
+                "alert_comparator": "greater than",
+                "alert_threshold": 5,
+            }
             response = requests.post(
                 upload_url,
                 headers=auth_header,
-                data={
-                    "name": rule.title,
-                    "search": "search " + rule.rule[0],
-                },
-                verify=False)
-            print(response.text)
-            print("\n")
+                data=schema,
+                verify=False
+            )
             if (response.status_code == 201):
                 print("Upload rule", rule.title, "successfully")
             else:
-                print("Error: cannot upload rule ", rule.title)
-    except:
-        print("Error: cannot connect to Splunk")
+                print("Upload rule", rule.title, "failed")
+
+    except Exception as err:
+        print(err)
+
+
+def api_search_job(rules, access_token):
+    try:
+        auth_header = {'Authorization': access_token}
+        upload_url = base_api_url + "/services/search/jobs?output_mode=json"
+        # for rule in rules:
+        if True:
+            schema = {
+                "name": "test-real-time",
+                "search": "search index=main eventtype=ms-sysmon-process",
+                "search_mode": "realtime",
+                "indexedRealtime": 1,
+                "indexedRealtimeOffset": 300,
+            }
+
+            response = requests.post(
+                upload_url,
+                headers=auth_header,
+                data=schema,
+                verify=False
+            )
+            if (response.status_code == 201):
+                print("Upload rule", "test-real-time", " successfully")
+            else:
+                print(response.content)
+    except Exception as err:
+        print(err)
 
 
 def api_update_search():
